a241f4441fc4b15a122034bef36b116cc3639bda
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-11-01 14:16:46 +0100

    Fix requirement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API and
-returns the number of subscribers (not active users,
-total subscribers) for a given subreddit. If an invalid
-subreddit is given, the function should return 0.
 """
-from json import JSONDecodeError
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function to get the number of subscribers"""
-    if type(subreddit) is not str or subreddit is None:
-        print()
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    try:
-        res = requests.get("https://www.reddit.com/r/{}/about.json"
-                           .format(subreddit), allow_redirects=False,
-                           headers={'User-Agent': 'My User Agent 1.0'}).json()
-    except JSONDecodeError:
-        return 0
-    subscribers = res.get('data', {}).get('subscribers', 0)
-    return subscribers
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by /u/aaorrico23)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
