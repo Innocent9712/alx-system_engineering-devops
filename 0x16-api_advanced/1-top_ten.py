dede6ee85bf6ac6c5eb673ee5c51d7df278da50c
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-11-01 14:24:26 +0100

    Fix bug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,15 +10,19 @@ import requests
 def top_ten(subreddit):
     """Function to get the top 10 post in the subreddit"""
     if type(subreddit) is not str or subreddit is None:
-        print()
+        print(None)
         return
     try:
-        res = requests.get("https://www.reddit.com/r/{}/top.json?limit=10"
+        res = requests.get("https://www.reddit.com/r/{}/top.json"
                            .format(subreddit), allow_redirects=False,
-                           headers={'User-Agent': 'My User Agent 1.0'}).json()
+                           headers={'User-Agent': 'My User Agent 1.0'},
+                           params={'limit': 10}).json()
     except JSONDecodeError:
         return print(None)
     top = res.get('data', {}).get('children', [])
+    if len(top) > 0 and top[0].get('kind') != 't3':
+        print(None)
+        return
     # for post in top:
     #     print (post.get('data', {}).get('title', ''))
-    return [print(post.get('data', {}).get('title', '')) for post in top]
+    return [print(post.get('data', {}).get('title', None)) for post in top]
