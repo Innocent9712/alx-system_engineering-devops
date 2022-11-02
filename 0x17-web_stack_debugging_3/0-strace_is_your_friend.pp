7b066ee7d453c9fe0b92a56438b01e42da8451af
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-11-02 13:54:04 +0100

    Fix requirement and directory name

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# this manifest corrects the php file extension in the WP settings file
 exec {'fix file extension':
   path    => ['/usr/bin', '/usr/local/bin', '/bin', '/usr/sbin'],
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
